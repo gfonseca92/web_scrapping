aa619ca8555cee81d4cb1ffd410770d16c4085cc
gfonseca92 <56279225+gfonseca92@users.noreply.github.com>
2021-01-08 02:54:36 -0300

    Colocando specs dos veículos e localização da loja

--- webcrawler.py
+++ webcrawler.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+import numpy as np
 from selenium.webdriver.common.keys import Keys
 import selenium.webdriver.chrome.service as service
 import smtplib
@@ -90,19 +91,61 @@ class WebMotorsScraper(WebScraper):
 
         time_list = []
         product_list = []
+        desc_list = []
+        loc_list = []
         price_list = []
+
         tag_list = self.driver.find_elements_by_tag_name("strong")
-        print(len(tag_list))
+        model_list = self.driver.find_elements_by_tag_name("h3")
+        location_list = self.driver.find_elements_by_tag_name("span")
+        model_dict = self.list_to_dict(model_list)
+        location_dict = self.list_to_dict(location_list)
+
         for item in tag_list:
             text = item.text
             price_text = text.replace("R$ ", "")
             if "R$ " in text:
+                pos = (float(item.location['x']), float(item.location['y']))
+                model = self.find_by_dist(pos, model_dict)
+                location = self.find_by_dist(pos, location_dict)
                 price_text = price_text.replace(".", "")
                 time_list.append(datetime.now())
                 product_list.append(self.manufacturer + "_" + self.model)
+                desc_list.append(model)
+                loc_list.append(location)
                 price_list.append(float(price_text))
-        self.price_df = pd.DataFrame([time_list, product_list, price_list]).transpose()
-        self.price_df.columns = ['Time', 'Product', 'Price']
+        self.price_df = pd.DataFrame([time_list, product_list, desc_list, loc_list, price_list]).transpose()
+        self.price_df.columns = ['Time', 'Product', 'Model', 'Location', 'Price']
+        print('oi')
+
+    def list_to_dict(self, input_list):
+        output_dict = {}
+        for item in input_list:
+            try:
+                output_dict[(float(item.location['x']), float(item.location['y']))] = item.text
+            except:
+                pass
+        return output_dict
+
+    def find_by_dist(self, pos, input_dict):
+        x0 = pos[0]
+        y0 = pos[1]
+        min_dist = 100000
+        key_model = 0
+        for key in input_dict.keys():
+            x1 = key[0]
+            y1 = key[1]
+            dist = np.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+            value = input_dict[key]
+            if dist < min_dist and len(value) != 9:
+                key_model = key
+                min_dist = dist
+        try:
+            result = input_dict[key_model]
+        except:
+            result = 'Não Identificado'
+        return result
+
 
 
 vehicles_df = pd.DataFrame()
@@ -110,13 +153,12 @@ path = './chromedriver.exe'
 url = 'https://www.webmotors.com.br/'
 wm = WebMotorsScraper(path, url)
 filters = {'store_type': 'Concessionária', 'fuel_type': 'Gasolina e álcool', 'category': 'Carros 1.0'}
-wm.search_new_vehicle('volkswagen', 'gol', **filters)
-vehicles_df = pd.concat([vehicles_df, wm.price_df])
+
 wm.search_new_vehicle('chevrolet', 'onix', **filters)
+vehicles_df = pd.concat([vehicles_df, wm.price_df])
+wm.search_new_vehicle('volkswagen', 'gol', **filters)
 vehicles_df = pd.concat([vehicles_df, wm.price_df])
 wm.search_new_vehicle('hyundai', 'hb20', **filters)
 vehicles_df = pd.concat([vehicles_df, wm.price_df])
 print(vehicles_df)
-
-
 
